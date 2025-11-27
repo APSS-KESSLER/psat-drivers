51ca506cab49481e861a81544946cc207b2b79a5
ivanf-nz <104700307+ivanf-nz@users.noreply.github.com>
2025-11-27 20:51:53 +1300

    add A0 and A1 uart

---
+++ board.h
@@ -0,0 +1,10 @@
+// Here is the header file for defining the specific UART and SPI pins
+
+// Table 6-14 
+//eUSCI_A0 - UART & SPI
+//eUSCI_A1 - UART & SPI
+//eUSCI_B0 & SPI
+//eUSCI_B1 & SPI
+
+//#define UART_A0 1
+#define UART_A1 1
---
+++ uart.h
@@ -0,0 +1,4 @@
+// Function Prototypes
+void setupUART(void);
+void printNumber(uint16_t num);
+void print(char text[]);
